99421e3911c1c70835f89bb20db2f5ec53372560
alexander_morozov-tl <alexander.morozov@travelline.ru>
2021-06-08 13:35:57 +0300

    2.14. Нужно больше функций Исправление замечаний https://up.htmlacademy.ru/javascript/23/tasks/5

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
-'use strict';
-
 const getRandomNumber = (min, max) => {
   if(min<0 || min > max) {
     throw new RangeError('Ошибка данных: первое значение должно быть меньше второго и быть >= 0');
   }
-  return Math.random() * (max - min) + min;
+  return Math.floor(Math.random() * (max + 1 - min) + min);
 };
 
-console.log( getRandomNumber(1,2));
+const getRandomReal = (min, max, decimalNumber) => {
+  if(min<0 || min > max) {
+    throw new RangeError('Ошибка данных: первое значение должно быть меньше второго и быть >= 0');
+  }
+  return (Math.random() * (max - min) + min).toFixed(decimalNumber);
+};
+
+getRandomNumber(1,2);
+getRandomReal(1.1,1.2, 2);
