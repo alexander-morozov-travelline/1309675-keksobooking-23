8a4c8e8a9a2fc71226f17b1199b45adf22073e5c
alexander_morozov-tl <alexander.morozov@travelline.ru>
2021-07-05 10:39:38 +0300

    5.12. Модуляция https://up.htmlacademy.ru/javascript/23/tasks/9

---
+++ js/util.js
@@ -0,0 +1,24 @@
+const getRandomNumber = (min, max) => {
+  if(min<0 || min > max) {
+    throw new RangeError('Ошибка данных: первое значение должно быть меньше второго и быть >= 0');
+  }
+  return Math.floor(Math.random() * (max + 1 - min) + min);
+};
+
+const getRandomReal = (min, max, decimalNumber) => {
+  if(min<0 || min > max) {
+    throw new RangeError('Ошибка данных: первое значение должно быть меньше второго и быть >= 0');
+  }
+  return (Math.random() * (max - min) + min).toFixed(decimalNumber);
+};
+
+const  getArrayRandElement = (arr) => {
+  const rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+};
+
+const shuffleArr = (arr) => arr.sort(() => Math.random() - 0.5);
+
+const getArrayRandLength = (arr) => shuffleArr(arr.slice(0, getRandomNumber(1, arr.length)));
+
+export {getRandomNumber, getRandomReal, getArrayRandElement, getArrayRandLength, shuffleArr};
